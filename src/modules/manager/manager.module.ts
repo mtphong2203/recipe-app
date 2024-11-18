b8b6c892b388a9e2d06243bb166fc0df4872c966
Tien Phong <tienphong2203@gmail.com>
2024-11-18 18:57:16 +0700

    create manager layout

---
+++ src/modules/manager/manager.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, RouterModule.forChild(routes)
+  ]
+})
+export class ManagerModule { }

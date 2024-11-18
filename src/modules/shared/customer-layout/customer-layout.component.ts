b634c674c042df99104aad01eb4450ed5356664d
Tien Phong <tienphong2203@gmail.com>
2024-11-18 18:32:06 +0700

    add footer and header to shared

--- src/modules/shared/customer-layout/customer-layout.component.ts
+++ src/modules/shared/customer-layout/customer-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-customer-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './customer-layout.component.html',
   styleUrl: './customer-layout.component.css'
 })
